01de15d031b988b9f8330029aad258ec36e1e703
Alex <alexrfarnes@gmail.com>
2021-07-19 16:36:22 +0800

    update ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import theme from "./theme";
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
+  <ThemeProvider theme={theme}>
     <CssBaseline />
     <App />
-  </MuiThemeProvider>,
+  </ThemeProvider>,
   document.getElementById("root")
 );
