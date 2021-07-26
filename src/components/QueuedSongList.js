75b6ef8ce2b3d6e20e99fdec5ac91833acb3c0db
Alex <alexrfarnes@gmail.com>
2021-07-26 21:31:09 +0800

    change queued songs key from index to song id

--- src/components/QueuedSongList.js
+++ src/components/QueuedSongList.js
@@ -19,8 +19,8 @@ function QueuedSongList({ queue }) {
         <Typography color='textSecondary' variant='button'>
           QUEUE ({queue.length})
         </Typography>
-        {queue.map((song, i) => (
-          <QueuedSong key={i} song={song} />
+        {queue.map(song => (
+          <QueuedSong key={song.id} song={song} />
         ))}
       </div>
     )
